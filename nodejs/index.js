c88ed99f751f8ce88e9e5ef18bf0991ff82b9f1b
sarazm2000 <63396470+sarazm2000@users.noreply.github.com>
2021-10-25 19:50:34 +0400

    Add files via upload

---
+++ nodejs/index.js
@@ -0,0 +1,53 @@
+var express = require('express')
+var cors = require('cors')
+var app = express()
+
+app.use(cors())
+
+
+
+app.listen(3000, function () {
+  console.log('CORS-enabled web server listening on port 80')
+})
+
+var bodyParser = require('body-parser');
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({
+    extended: true
+  }));
+// app.use('/', router);
+var crypto = require('crypto');
+
+const redis = require("redis");
+const client = redis.createClient();
+
+app.post('/', function (req, res, next) {
+console.log(req.body.input);
+if (req.body.input.length < 8){
+    return ;
+}
+var hashed = crypto.createHash('sha256').update(req.body.input).digest('base64');
+client.set( hashed, req.body.input, redis.print);
+
+// client.get("hashedInput", redis.print)
+res.send({
+    "message" : hashed,
+});
+})
+
+app.get('/', function(req, res, next){
+  const input = req.query.input;
+  console.log(req.query.input)
+  var hashedString;
+  client.get(input , function(err, reply) {
+    hashedString = reply;
+    console.log(hashedString);
+    res.send({
+      "message" : hashedString,
+  });
+  });
+ 
+})
